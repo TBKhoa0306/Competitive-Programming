0fffe0c8fc9c2c1ea9541546743d6d357ffdc001
Bao Khoa <Khoa@MacBook-Pro.local>
2025-11-02 00:03:22 +0700

    add kiet and test

---
+++ kiet.cpp
@@ -0,0 +1,13 @@
+/**
+ * @author      : Khoa
+ * @file        : "kiet.cpp"
+ * @created     : "2025-11-02 00:02"
+ */
+#include <iostream>
+using namespace std; 
+
+int main()
+{
+    
+    return 0;
+}
---
+++ test.cpp
@@ -0,0 +1,13 @@
+/**
+ * @author      : Khoa
+ * @file        : "test.cpp"
+ * @created     : "2025-11-02 00:01"
+ */
+#include <iostream>
+using namespace std; 
+
+int main()
+{
+    
+    return 0;
+}
