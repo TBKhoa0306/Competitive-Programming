c144ae57551771b0f7c496c6e7f0b1a9187d06d9
Bao Khoa <Khoa@MacBook-Pro.local>
2025-11-02 00:02:13 +0700

    create prime.cpp

---
+++ prime.cpp
@@ -0,0 +1,13 @@
+/**
+ * @author      : Khoa
+ * @file        : "prime.cpp"
+ * @created     : "2025-11-02 00:00"
+ */
+#include <iostream>
+using namespace std; 
+
+int main()
+{
+    
+    return 0;
+}
